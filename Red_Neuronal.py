890575782620203fc481a3285cf5bad182a1200b
RobertoVazquez7 <tlfcrazzyd4rk@gmail.com>
2023-03-01 23:40:31 -0600

    actualizacion ultima grafica

--- Red_Neuronal.py
+++ Red_Neuronal.py
@@ -8,18 +8,16 @@ from keras import layers
 from keras.models import Sequential
 import seaborn as sns
 from tkinter import *
+from sklearn.model_selection import cross_val_score
+from keras.wrappers.scikit_learn import KerasClassifier
 
-ventana = Tk()
-ventana.title('IA - Actividad 3, Corte 2')
-ventana.geometry('400x200')
+batch_size = 180 # cantidad de imagenes
+img_height = 100 # alto
+img_width = 100 # ancho
 
 def main():
 
     epocas = int(cantidad_epocas.get())
-
-    batch_size = 180 # cantidad de imagenes
-    img_height = 100 # alto
-    img_width = 100 # ancho
 
     train_ds = tf.keras.utils.image_dataset_from_directory(
         "./data/Entrenamiento",
@@ -36,6 +34,7 @@ def main():
     val_ds = val_ds.cache().prefetch(buffer_size=10)
 
     num_clases = len(class_names)
+
     data_augmentation = tf.keras.Sequential([tf.keras.layers.RandomFlip("horizontal",input_shape=(img_height,img_width,3)),
         tf.keras.layers.RandomRotation(0.1),
         tf.keras.layers.RandomZoom(0.1),])
@@ -56,8 +55,11 @@ def main():
     modelo.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
+    
+    def retorno_modelo():
+        return modelo
+
     modelo.summary()
-    #epochs = 50
     history = modelo.fit(
     train_ds,
     validation_data=val_ds,
@@ -89,7 +91,7 @@ def main():
             predictions = modelo.predict(images)
             score = tf.nn.softmax(predictions[i])
             plt.imshow(images[i].numpy().astype("uint8"))
-            plt.title('Clase: {} | Precisión: {:.2f}'.format(class_names[np.argmax(score)], 100 * np.max(score)))
+            plt.title('Clase: {} | Precisión: % {:.2f}'.format(class_names[np.argmax(score)], 100 * np.max(score)))
             plt.show()
             break
 
@@ -98,6 +100,8 @@ def main():
     for img, labels in val_ds.take(1):
         test_images.append(img)
         test_labels.append(labels)
+        imgAux = np.array(img)
+        labelsAux = np.array(labels)
 
     y_pred = np.argmax(modelo.predict(test_images), axis=1).flatten()
     y_true = np.asarray(test_labels).flatten()
@@ -113,6 +117,21 @@ def main():
     plt.xlabel('Predicciones')
     plt.ylabel('Datos')
     plt.show()
+
+#validación cruzada k-fold de 9 partes.
+    keras_model = KerasClassifier(build_fn = retorno_modelo, epochs = epocas , batch_size=180)
+    scores = cross_val_score(keras_model, imgAux, labelsAux, cv=5)
+    mean_score = np.mean(scores)
+    plt.bar(range(len(scores)), scores)
+    plt.axhline(y = mean_score, color='r', linestyle='-')
+    plt.xlabel('Fold')
+    plt.ylabel('Score')
+    plt.title('Cross-validation scores')
+    plt.show()
+
+ventana = Tk()
+ventana.title('IA - Actividad 3, Corte 2')
+ventana.geometry('400x200')
 
 label = Label(ventana, text='Ingrese cantidad de epocas de entrenamiento:').place(x=80, y=10)
 
